72fe77a8a69ddb139edf63c48f0e6fa9333fc49f
Adam Steger <70110948+adamsteger@users.noreply.github.com>
2023-04-17 19:40:26 -0400

    Removed print statement

--- MyNFASimulator.java
+++ MyNFASimulator.java
@@ -5,7 +5,6 @@ public class MyNFASimulator {
     public static void main(String[] args) {
         File file = new File(args[0]);
         NFA nfa = NFA.readFile(file);
-        System.out.println(nfa);
         Scanner scanner = new Scanner(System.in);
         while(true) {
             String input = scanner.nextLine();
